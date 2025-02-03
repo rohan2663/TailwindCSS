1f17e9e6861a6ee082f4278291426df3a2db3994
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2025-02-03 17:15:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    container:{
+      padding:'10rem',
+      center:true
+    },
+    extend: {},
+  },
+  plugins: [],
+};
